572f8d783744e679740c9221f53d1fb8768f8c34
Ramirez-05 <117038114+Ramirez-05@users.noreply.github.com>
2024-11-21 16:04:55 -0500

    Se realizo el home y composicion del nav

--- Angular/prueba-tecnica/src/app/app.routes.ts
+++ Angular/prueba-tecnica/src/app/app.routes.ts
@@ -1,3 +1,11 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TareasComponent } from './tareas/tareas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'tareas', component: TareasComponent },
+    { path: '**', redirectTo: 'home' }
+];
---
+++ Angular/prueba-tecnica/src/app/tareas/tareas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-tareas',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './tareas.component.html',
+    styleUrls: ['./tareas.component.css']
+})
+export class TareasComponent {}
