0a28e7acc7bfc4fc517732cc03a9569d459f54d6
Ramirez-05 <117038114+Ramirez-05@users.noreply.github.com>
2024-11-22 17:36:55 -0500

    Update API endpoints to use production URLs

--- Angular/prueba-tecnica/src/app/api/constants.ts
+++ Angular/prueba-tecnica/src/app/api/constants.ts
@@ -1,13 +1,13 @@
 // api/api-endpoints.ts
 export const API_ENDPOINTS = {
     tareas: {
-      base: 'http://localhost:8000/tarea',
+      base: 'https://mowin.onrender.com/tarea',
     },
     personas: {
-      base: 'http://localhost:8000/persona',
+      base: 'https://mowin.onrender.com/persona',
     },
     tareaProgramada: {
-      base: 'http://localhost:8000/tareaProgramada'
+      base: 'https://mowin.onrender.com/tareaProgramada'
 
     },
   };
